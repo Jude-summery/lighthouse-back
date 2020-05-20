3b6251e3c8505fa96e14d0946824c88969727845
Jude-summery <329543240@qq.com>
2020-05-20 10:35:49 +0800

    update：配置路由；配置启动脚本

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('index');
-});
-
-module.exports = router;
+/* 路由配置 */
+module.exports = function (app) {
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' })
+  })
+  app.use('/api/login', require('./login'))
+  app.use('/api/logout', require('./logout'))
+  app.use('/api/users', require('./users'))
+  app.use('/api/groups', require('./groups'))
+  app.use('/api/tasks', require('./tasks'))
+}
